3380e7de8022b57c77dc0fd31b42a7191a83492d
Felipe <felipeiasi@gmail.com>
2019-01-08 14:53:18 -0200

    Notebook Analise

--- scripts/download.py
+++ scripts/download.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 cicloList = [0,6,12,18]
 
-def download(data=None, ciclo=0, quantidadeHoras=1, step=1):
+def download(data=None, ciclo=0, quantidadeHoras=120, step=3):
     if data == None:
         data = datetime.now().strftime('%Y%m%d')
     if ciclo not in cicloList:
@@ -15,7 +15,8 @@ def download(data=None, ciclo=0, quantidadeHoras=1, step=1):
         urlNoaa="http://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.{}{:02}/gfs.t{:02}z.pgrb2.0p25.f{:03}".format(data, ciclo, ciclo, i)
         fileName=urlNoaa.split('/')[-1]
         print("Downloading: {}".format(fileName))
-        r = requests.get("grib/{}".format(urlNoaa), allow_redirects=True)
-        open(fileName, 'wb').write(r.content)
+        r = requests.get(urlNoaa, allow_redirects=True)
+        open("../grib/data_{:02}/{}".format(ciclo,fileName), 'wb').write(r.content)
 
-download()
+if __name__ == "__main__":
+    download()
