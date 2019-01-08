41eff5146cee95ad4cd27d8abe7c109671b7fd38
Felipe Costa <felipeiasi@gmail.com>
2019-01-08 09:48:17 -0200

    Script Download Files

--- scripts/download.py
+++ scripts/download.py
@@ -1,4 +1,6 @@
 import requests
+import argparse
+import sys
 from datetime import datetime
 
 cicloList = [0,6,12,18]
@@ -9,7 +11,11 @@ def download(data=None, ciclo=0, quantidadeHoras=1, step=1):
     if ciclo not in cicloList:
         raise ValueError('Ciclo deve ser 0, 6, 12 ou 18')
     for i in range(0, quantidadeHoras, step):
-        print(i)
-    urlNoaa="http://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.2019010718/gfs.t18z.pgrb2.0p25.f000"
+
+        urlNoaa="http://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.{}{:02}/gfs.t{:02}z.pgrb2.0p25.f{:03}".format(data, ciclo, ciclo, i)
+        fileName=urlNoaa.split('/')[-1]
+        print("Downloading: {}".format(fileName))
+        r = requests.get("grib/{}".format(urlNoaa), allow_redirects=True)
+        open(fileName, 'wb').write(r.content)
 
 download()
